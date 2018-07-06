bd92085b3be4ebec3847f0a47d02dd2be06db350
Elena <hel-kovalyova@yandex.ru>
2018-07-06 10:20:27 +0300

    lesson2 with function

--- Lesson2/script.js
+++ Lesson2/script.js
@@ -1,19 +1,5 @@
-
-alert("Задание на выведение простых чисел на экран в заданном интервале");
-var start,end;
-	do
-	{
-		start=prompt("Введите целое число не меньше единицы - начало интервала");
-	} while (start<1 || (start-Math.trunc(start))>0);
-
-	do
-	{
-		end=prompt("Введите целое число - конец интервала");
-	} while (end<1 || (end-Math.trunc(end)>0) || start>=end);
-
-var i;
-var k=1;
-var numbers;
+"use strict";
+function ShowSimpleNumber(){
 do
 {
 	for(i=2;i<=start;i++)
@@ -26,3 +12,22 @@ do
 	start++;
 	k=1;
 }while(start<=end);
+
+
+}
+alert("Задание на выведение простых чисел на экран в заданном интервале");
+var start,end;
+	do
+	{
+		start=prompt("Введите целое число не меньше единицы - начало интервала");
+	} while (start<1 && (start-Math.trunc(start))>0);
+
+	do
+	{
+		end=prompt("Введите целое число - конец интервала");
+	} while (end<1 && (end-Math.trunc(end)>0) && start>=end);
+
+var i;
+var k=1;
+var numbers;
+ShowSimpleNumber();
